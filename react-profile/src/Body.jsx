306693bedba1f8a501788b2a07c457ad60bca921
lacson_c <lacson_c@f112.lan>
2024-09-06 13:54:02 +0800

    Adding my react profile (jsx)

---
+++ react-profile/src/Body.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import profilePicture from './assets/Carr.png';
+
+export default function Body() {
+  return (
+    <div className="person">
+      <img src="src/assets/Carr.png" alt="Description of the image" />
+      <p>
+        I am Carrene Kyla Martin Lacson, dedicatedly pursuing a Bachelor of Science in Computer Science
+        at the esteemed University of Baguio. My academic journey has been marked by notable achievements,
+        as evidenced by certificates acknowledging my Outstanding Performance in ICT, work immersion, and research,
+        along with the distinction of achieving high honors.
+      </p>
+      <p>
+        Beyond the realm of academia, I am a well-rounded individual with diverse interests. Sports play a significant
+        role in my life, and I find fulfillment in activities such as boxing, golf, and volleyball. Engaging in these
+        pursuits not only provides a healthy balance to my academic endeavors but also contributes to my overall personal
+        development.
+      </p>
+      <p>
+        In addition to my academic and athletic pursuits, I am continuously seeking opportunities for growth and learning.
+        As I navigate through my computer science studies, I am driven by a passion for innovation and a commitment to making
+        a positive impact in the ever-evolving field of technology.
+      </p>
+    </div>
+  );
+}
---
+++ react-profile/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+export default function Footer(){
+    return (
+      <footer>
+        <p>
+          &copy; {new Date().getFullYear()} All rights reserved || Carrene Kyla M. Lacson
+        </p>
+      </footer>
+    );
+  }
