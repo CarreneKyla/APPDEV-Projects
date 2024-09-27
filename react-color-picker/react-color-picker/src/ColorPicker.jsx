b2cd43e31db79d4cf0069c3a0ab9a529e5ff2f8d
Carrene Lacson <20211473@s.ubaguio.edu>
2024-09-27 13:17:39 +0800

    Update ColorPicker.jsx

--- react-color-picker/react-color-picker/src/ColorPicker.jsx
+++ react-color-picker/react-color-picker/src/ColorPicker.jsx
@@ -36,7 +36,7 @@ export default function ColorPicker (){
         <div className="color-picker-container">
             <h1>Color Picker</h1>
             <div className="color-display" style={{backgroundColor: color}}>
-                <p>Selected Color: </p>
+                <p>Selected Color: {color}</p>
             </div>
             <label>Select a Color: </label>
             <input type="color" value = {color} onChange = {handleColorChange}/>
